2132a5784e543f66bfbea97be19f5eee678f7244
Anita Angelidou <anitawebdesigns@gmail.com>
2023-04-16 09:59:42 +0300

    corrected: localhost3636 in axios

--- client/src/CreateArticle.js
+++ client/src/CreateArticle.js
@@ -9,7 +9,7 @@ function Create() {
   const navigate = useNavigate();
 
   function save(e) {
-    axios.post("http://localhost:3000/articles/", { title, description, text });
+    axios.post("http://localhost:3636/articles/", { title, description, text });
     navigate("/blog");
   }
 
